410b765df12afa390f60a5c88e3472db4c2e1b7a
Cubel <p.cubel@gmail.com>
2021-02-14 16:32:27 +0100

    Tarea 2

---
+++ tarea2/app/src/main/java/dev/cubel/campusmoba/tarea2/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.cubel.campusmoba.tarea2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
